6de243a57840d66b29df17febaac192ffd6c0369
Doan Ngoc Thuong <45509523+ngthuongdoan@users.noreply.github.com>
2020-10-16 14:43:54 +0700

    submenu for about me

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+//SHOW SIDEBAR
 const hoverBar = document.getElementsByClassName("hover-bar")[0];
 const hamburger = document.getElementById("hamburger");
 
@@ -14,4 +15,27 @@ main.addEventListener("mouseenter", () => {
   sidebar.style.marginLeft = "-500px";
   hamburger.style.visibility = "visible";
   main.style.transform = "none";
+});
+
+//SUBMENU
+const aboutSubmenu = document.getElementById("about__submenu");
+const aboutMenu = document.getElementById("about__menu");
+
+let isHover = false;
+aboutMenu.addEventListener("mouseover", () => {
+  if (!isHover) {
+    isHover = true;
+    gsap.from(aboutSubmenu, {
+      opacity: 0,
+      height: 0,
+      duration: 2,
+      ease: "power3.out",
+    });
+    aboutSubmenu.style.display = "block";
+  }
+});
+
+aboutSubmenu.addEventListener("mouseleave", () => {
+  isHover = false;
+  aboutSubmenu.style.display = "none";
 });
